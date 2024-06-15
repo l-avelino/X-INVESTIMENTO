2b8c4fc8ed99f00b5bc88fc96977e547728a1005
Luis Alberto <135447291+l-avelino@users.noreply.github.com>
2024-06-15 15:05:49 -0300

    Add files via upload

---
+++ java/br/com/caelum/vraptor/controller/ExtratoController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("extrato")
+@Controller
+public class ExtratoController {
+
+	@Get("")
+	public void extrato() {
+		
+	}
+}
---
+++ java/br/com/caelum/vraptor/controller/RecsenhaController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("recsenha")
+@Controller
+public class RecsenhaController {
+
+	@Get("")
+	public void recsenha() {
+		
+	}
+}
